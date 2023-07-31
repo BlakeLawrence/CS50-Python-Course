d61b832a87af72e7e73199189394a0e39c29e8da
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-07-31 08:00:16 +0100

    integers basics

---
+++ part2-numbers.py
@@ -0,0 +1,11 @@
+# calculator
+# unnecessary z variable - longer way
+# x = input("Whats X?")
+# y = input("Whats Y?")
+# z = int(x)+int(y)
+# print(z)
+
+# gets rid oz z variable - cleaner way
+x = int(input("Whats X?"))
+y = int(input("Whats Y?"))
+print(x+y)
