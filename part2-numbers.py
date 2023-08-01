f4508f18f2ab5ea99eedfa2b7a8b4bfec95effd2
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-08-01 19:18:42 +0100

    floats, round and formatting numbers

--- part2-numbers.py
+++ part2-numbers.py
@@ -6,6 +6,21 @@
 # print(z)
 
 # gets rid oz z variable - cleaner way
+# x = int(input("Whats X?"))
+# y = int(input("Whats Y?"))
+# print(x+y)
+
+# float - number with a decimal (allows decimals, int will not)
+# x = float(input("Whats X?"))
+# y = float(input("Whats Y?"))
+# print(x+y)
+
+# round will round up or down
+# print(round(1.4)) #1
+# print(round(1.5)) #2
+
+# number formatting - f string (format)
 x = int(input("Whats X?"))
 y = int(input("Whats Y?"))
-print(x+y)
+z = x+y
+print(f"{z:,}")
