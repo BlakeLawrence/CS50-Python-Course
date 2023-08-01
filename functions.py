5919235e1b6076da4b447ef16b4f85886ccad91d
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-08-01 19:42:40 +0100

    functions basics

---
+++ functions.py
@@ -0,0 +1,9 @@
+# Functions
+# def is short for define and is used for creating functions
+# everything after the colon is the function body
+def hello(to="world"):
+    print("Hello,", to)
+
+hello()
+name = input("Whats your name?")
+hello(name)
